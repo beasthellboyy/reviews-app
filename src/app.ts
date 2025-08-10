30821bb155cf82885fc20c464a976da038d1a929
Tanish Sharma <125534048+beasthellboyy@users.noreply.github.com>
2025-08-10 19:34:46 +0530

    Create app.ts

---
+++ src/app.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import { setReviewRoutes } from './routes/reviewRoutes';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+
+setReviewRoutes(app);
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
